1186c417ff6f9d171fa70b7de6d9dbdb38982658
JavierFernandez136 <javier_fernandez_96@outlook.com>
2025-05-30 18:17:31 -0300

    Agregado de recetas en recetario de produccion

--- src/server/produccion.js
+++ src/server/produccion.js
@@ -1,4 +1,3 @@
-// src/server/produccion.js
 
 const produccion = [
     {
@@ -60,6 +59,96 @@ const produccion = [
       ingredientes: "500g frutillas, 300ml vinagre de manzana, 300g azúcar",
       instrucciones: "Macerar las frutillas con azúcar 24h, luego agregar el vinagre. Filtrar y embotellar.",
       imagen: "/images/shrub_frutilla.jpg"
+    },
+    {
+      nombre: "Gajos de Pomelo",
+      ingredientes: "200ml jugo de pomelo, 100g azúcar, 20g gelatina sin sabor, 2g agar agar",
+      instrucciones: "Cortar pomelos y hacer jugo. Hidratar gelatina y agar con 100ml de agua. Cocinar jugo con azúcar, agregar gelatina hidratada, romper hervor. Colocar en cáscaras limpias, enfriar 40 min y cortar en gajos.",
+      imagen: "/images/gajos_pomelo.jpg"
+    },
+    {
+      nombre: "Gomitas Kumquat",
+      ingredientes: "50ml kumquat, 50ml agua, 75g azúcar, 15g gelatina, 2g ácido tartárico",
+      instrucciones: "Evaporar alcohol del kumquat. Agregar agua, azúcar y ácido. Hidratar gelatina con 75ml agua. Integrar, calentar hasta hervor. Moldar, enfriar 40 min y desmoldar.",
+      imagen: "/images/gomitas_kumquat.jpg"
+    },
+    {
+      nombre: "Chocolate Matcha",
+      ingredientes: "300g chocolate blanco, 2g matcha",
+      instrucciones: "Derretir chocolate a baño maría. Agregar matcha, integrar. Colocar en manga, llenar moldes, enfriar y desmoldar.",
+      imagen: "/images/chocolate_matcha.jpg"
+    },
+    {
+      nombre: "Sabayon",
+      ingredientes: "200g yemas, 200g azúcar, 190ml vino tardío, 10ml Frangelico",
+      instrucciones: "Batir yemas con azúcar a baño maría. Agregar vinos. Cocinar 2 minutos extra. Enfriar y embotellar.",
+      imagen: "/images/sabayon.jpg"
+    },
+    {
+      nombre: "Rosas de Manzana",
+      ingredientes: "180g manzana, 60g azúcar",
+      instrucciones: "Cortar manzana en fetas finas. Cocinar con caramelo hasta ablandar. Enrollar fetas y deshidratar 8hs.",
+      imagen: "/images/rosas_manzana.jpg"
+    },
+    {
+      nombre: "Jack Macerado con Cookies",
+      ingredientes: "1L Jack Daniel's, 340g cookies",
+      instrucciones: "Macerar Jack con cookies y Jen Bean en freezer. Filtrar, mezclar con lima, cacao, harina. Estirar, hornear, conservar en tupper.",
+      imagen: "/images/jack_cookies.jpg"
+    },
+    {
+      nombre: "Cordial de California",
+      ingredientes: "5u té rojo, 500ml agua, 20g hibiscus, 250g azúcar, 5g ácido tartárico",
+      instrucciones: "Hervir todo, reposar, colar y refrigerar.",
+      imagen: "/images/cordial_california.jpg"
+    },
+    {
+      nombre: "Sidra",
+      ingredientes: "700ml jugo de pera, 350ml jugo manzana verde, 350ml jugo manzana roja, 50g bagazo c/u, 50g azúcar",
+      instrucciones: "Mezclar todo, dejar fermentar 7 días. Filtrar, embotellar y refrigerar.",
+      imagen: "/images/sidra.jpg"
+    },
+    {
+      nombre: "Cordial de Matcha",
+      ingredientes: "5g manzanilla, 2g matcha, 125ml jugo manzana verde, 125ml agua, 250g azúcar, 3g ácido tartárico",
+      instrucciones: "Hervir todo por 5 min. Reposar, filtrar, embotellar y refrigerar.",
+      imagen: "/images/cordial_matcha.jpg"
+    },
+    {
+      nombre: "Tintura de Evas",
+      ingredientes: "330g frutos rojos, 100g azúcar, 350ml vodka",
+      instrucciones: "Macerar todo 1 semana. Filtrar con lienzo. Conservar en frasco.",
+      imagen: "/images/tintura_evas.jpg"
+    },
+    {
+      nombre: "Shrub Manzana y Pera",
+      ingredientes: "200g manzana verde, 200g pera, 800g azúcar, 400ml vinagre de manzana, 100ml jugo de manzana verde",
+      instrucciones: "Licuar frutas con vinagre y jugo. Reposar 7 días en frío. Filtrar sin presionar, embotellar.",
+      imagen: "/images/shrub_fruta.jpg"
+    },
+    {
+      nombre: "Kirsch Casero",
+      ingredientes: "350g cerezas, 125g azúcar, 125g agua, 70ml vodka, 150g ron blanco",
+      instrucciones: "Macerar todo 8 días, mezclando a diario. Filtrar y conservar en heladera.",
+      imagen: "/images/kirsch.jpg"
+    },
+    {
+      nombre: "Tintura de Lavanda",
+      ingredientes: "5g lavanda seca, 30ml vodka",
+      instrucciones: "Macerar 1 semana. Filtrar y conservar.",
+      imagen: "/images/tintura_lavanda.jpg"
+    },
+    {
+      nombre: "Tintura de Eucalipto",
+      ingredientes: "10g hojas de eucalipto, 650ml vodka",
+      instrucciones: "Macerar 1 semana. Filtrar y conservar.",
+      imagen: "/images/tintura_eucalipto.jpg"
+    },
+    {
+      nombre: "Cordial de Naranja",
+      ingredientes: "Pieles y jugo de naranja (200ml), 50g azúcar, 2 ramas canela, 3 clavos de olor, 2g ácido tartárico",
+      instrucciones: "Hervir todo. Colar, embotellar y refrigerar.",
+      imagen: "/images/cordial_naranja.jpg"
     }
   ];
   
